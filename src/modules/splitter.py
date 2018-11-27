db12f46f3abc875eeea405deac0639b0752dbdd8
abhinavds <abhinav.ds@utexas.com>
2018-11-26 22:17:53 -0600

    Splitter module code written.
    Signed-off-by: abhinavds <abhinav.ds@utexas.com>

--- src/modules/splitter.py
+++ src/modules/splitter.py
@@ -9,57 +9,49 @@ class Splitter(nn.Module):
 		super(Splitter, self).__init__()
 		self.image_width = image_width
 		self.dim_size = dim_size #Coordinate dimension
-		self.layers1 = nn.ModuleList()
-		self.layers2 = nn.ModuleList()
-		self.layers3 = nn.ModuleList()
-		# self.add_layer(self.layers1, torch.nn.Conv1d(i))
-		# self.add_layer(self.layers2, nn.Linear(2*feature_size,feature_size))
-		# self.add_layer(nn.Linear(2*feature_size,feature_size))
-		# for i in range(depth):
-		# 	self.add_layer(nn.Linear(self.feature_size,self.feature_size))
-		# 	self.add_layer(nn.Linear(self.feature_size,self.feature_size))
-		# self.W_p_c = nn.Linear(self.dim_size,self.feature_size)
-		# self.W_p_s = nn.Linear(self.feature_size,self.feature_size)
-		# self.W_p = nn.Linear(2*self.feature_size,self.feature_size)
-		# self.W_c = nn.Linear(self.feature_size,self.dim_size)
-		# self.W_ic = nn.Linear(self.dim_size,self.feature_size)
-		# self.W_lol = nn.Linear(self.feature_size,self.dim_size)
-		# self.a = nn.Tanh()
-		# # Initialize weights according to the Xavier Glorot formula
-		# nn.init.xavier_uniform_(self.W_p_c.weight)
-		# nn.init.xavier_uniform_(self.W_p_s.weight)
-		# nn.init.xavier_uniform_(self.W_p.weight)
-		# nn.init.xavier_uniform_(self.W_c.weight)
-		# nn.init.xavier_uniform_(self.W_ic.weight)
-		# nn.init.xavier_uniform_(self.W_lol.weight)
+		self.activation = nn.ReLU()
 
+		self.feature_layers = nn.ModuleList()
+		self.add_layer(self.feature_layers, nn.Conv1d(1, 16, 3)) # n_channels, out_channels, kernel_size
+		self.add_layer(self.merge_layers, self.activation, init=False)
+		self.add_layer(self.feature_layers, nn.MaxPool1d(2))
+		self.add_layer(self.feature_layers, nn.Conv1d(16, 32, 3)) # n_channels, out_channels, kernel_size
+		self.add_layer(self.merge_layers, self.activation, init=False)
+		self.add_layer(self.feature_layers, nn.MaxPool1d(2))
+		self.add_layer(self.feature_layers, nn.Conv1d(32, 64, 3)) # n_channels, out_channels, kernel_size
+		self.add_layer(self.merge_layers, self.activation, init=False)
+		self.add_layer(self.feature_layers, nn.MaxPool1d(2))
+
+
+		self.merged_layers = nn.ModuleList()
+		self.add_layer(self.merged_layers, nn.Linear(int(self.image_width/8),int(self.image_width/8)))
+		self.add_layer(self.merged_layers, self.activation, init=False)
+		self.add_layer(self.merged_layers, nn.Linear(int(self.image_width/8), dim_size*2 + 1))
+		self.add_layer(self.merged_layers, self.activation, init=False)
+
+
+		
 	def add_layer(self, layers, layer,init=True):
 		layers.append(layer)
 		if init:
 			nn.init.xavier_uniform_(layers[-1].weight)
 
 	def forward(self, proj_c, proj_gt):
-		#proj_c: batch_size x image_width_1d
-		#proj_gt: batch_size x image_width_1d
-		
-		# temp_A = Variable(torch.Tensor(A).type(dtype),requires_grad=False)
-		# # #s = self.a(self.W_s(s_prev) + self.W_x(x_prev))
-		
-		# c_f = self.a(self.W_p_c(c_prev))
-		# s_f = self.a(self.W_p_s(s_prev))
-		# feature_from_state = self.a(self.W_p(torch.cat((c_f,s_f),dim=2)))
-		# # #concat_feature = torch.cat((c_prev,s_prev),dim=1)
-		# # #feature_from_state = self.a(self.W_p_2(self.a(self.W_p_1(concat_feature))))
-		# x = torch.cat((feature_from_state,x_prev),dim=2)
-		# x = self.a(self.layers[0](x)+torch.bmm(temp_A,self.layers[1](x)))
-		# for i in range(2,len(self.layers),2):
-		# 	x = self.a(self.layers[i](x)+torch.bmm(temp_A,self.layers[i+1](x)) + x)
-		# #c = self.a(self.W_c(x)+c_prev)
-		# # c = self.a(self.W_p_s(s_prev[0,:]))#+c_prev)
-		# c = self.a(self.W_lol(x))
-		# s = s_prev
-		# #print(x)
-		# #c = c.view((-1,2))
+		"""
+		proj_c: batch_size x image_width_1d
+		proj_gt: batch_size x image_width_1d
+		x: batch_size x 4
+		"""
+
+		x1 = proj_c
+		x2 = proj_gt
+		for layer in self.feature_layers:
+			x1 = layer(x1)
+			x2 = layer(x2)
+		x = torch.cat((x1,x2), dim=1)
+		for layer in self.merged_layers:
+			x = layer(x)
+			
 		return x
 
 	def calculateReward(points, c, A, gt):
