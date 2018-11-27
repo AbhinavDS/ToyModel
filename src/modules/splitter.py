ad994b23cde31aa71f12bcdbf9c8224ffd0b28aa
udaykusupati <kusupatiuday@gmail.com>
2018-11-26 23:12:44 -0600

    Reward code

--- src/modules/splitter.py
+++ src/modules/splitter.py
@@ -54,6 +54,55 @@ class Splitter(nn.Module):
 			
 		return x
 
-	def calculateReward(points, c, A, gt):
-		x1, y1, x2, y2 = points
-		return 1
+	def calculateReward(points, c, Pid, gt, mask):
+		p1, q1, p2, q2 = points
+		batch_size = c.size(0)
+		reward = torch.zeros((batch_size),dtype=dtype)
+		num_intersections = 0
+		edges = []
+		for b in range(batch_size):
+			num_verts = Pid[b].shape[0]
+			for i in range(num_verts):
+				if num_intersections > 2:
+					break
+				for j in range(i,num_verts):
+					if num_intersections > 2:
+						break
+					if Pid[b][i,j]:
+						x1, y1, x2, y2 = c[b,i,0].item(),c[b,i,1].item(),c[b,j,0].item(),c[b,j,1].item()
+						if(intersect(p1,q1,p2,q2,x1,y1,x2,y2)):
+							num_intersections += 1
+							edges.append([i,j])
+			if num_intersections != 2:
+				reward[b] = 0
+				continue
+			else if Pid[b][edges[0][0],edges[0][1]] != Pid[b][edges[1][0],edges[1][1]]:
+				reward[b] = 0
+				continue
+			else:
+				masked_gt = gt[b].masked_select(mask[b].unsqueeze(1).repeat(1,self.dim_size)).reshape(-1, self.dim_size)
+				pos = 0
+				neg = 0
+				for i in range(masked_gt.size(0)):
+					if masked_gt[i,0].item() == -2:
+						continue
+					x1, y1, x2, y2 = masked_gt[i,0].item(), masked_gt[i,1].item(), masked_gt[(i+1)%masked_gt.size(0),0].item(), masked_gt[(i+1)%masked_gt.size(0),0].item()
+					if line(p1,q1,p2,q2,x1,y1) > 0:
+						pos += 1
+					else:
+						neg += 1
+					if(intersect(p1,q1,p2,q2,x1,y1,x2,y2)):
+						reward[b] = 0
+						break
+					else:
+						reward[b] = 1
+				if(pos == 0 or neg == 0):
+					reward[b] = 0
+		return reward
+
+	def line(p1,q1,p2,q2,x1,y1):
+		return (p2-p1)*y1 - (q2-q1)*x1 -(q1*p2-q2*p1)
+
+	def intersect(p1,q1,p2,q2,x1,y1,x2,y2):
+		#x1,y1 substituted in line joining p1,q1, p2,q2 is line(p1,q1,p2,q2,x1,y1)
+		return line(p1,q1,p2,q2,x1,y1)*line(p1,q1,p2,q2,x2,y2) < 0 and line(x1,y1,x2,y2,p1,q1)*line(x1,y1,x2,y2,p2,q2) < 0
