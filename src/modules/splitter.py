998e536422a3210f04c05d36fbe088b99f340a2c
abhinavds <abhinav.ds@utexas.com>
2018-11-26 22:41:53 -0600

    Value predicted after prediction of line.
    Signed-off-by: abhinavds <abhinav.ds@utexas.com>

--- src/modules/splitter.py
+++ src/modules/splitter.py
@@ -22,15 +22,13 @@ class Splitter(nn.Module):
 		self.add_layer(self.merge_layers, self.activation, init=False)
 		self.add_layer(self.feature_layers, nn.MaxPool1d(2))
 
+		self.merged_fc = nn.Linear(int(self.image_width/8),int(self.image_width/8))
+		self.output_line = nn.Linear(int(self.image_width/8), dim_size*2)
+		self.output_value = nn.Linear(int(self.image_width/8) + dim_size*2, 1)
+		nn.init.xavier_uniform_(self.merged_fc.weight)
+		nn.init.xavier_uniform_(self.output_line.weight)
+		nn.init.xavier_uniform_(self.output_value.weight)
 
-		self.merged_layers = nn.ModuleList()
-		self.add_layer(self.merged_layers, nn.Linear(int(self.image_width/8),int(self.image_width/8)))
-		self.add_layer(self.merged_layers, self.activation, init=False)
-		self.add_layer(self.merged_layers, nn.Linear(int(self.image_width/8), dim_size*2 + 1))
-		self.add_layer(self.merged_layers, self.activation, init=False)
-
-
-		
 	def add_layer(self, layers, layer,init=True):
 		layers.append(layer)
 		if init:
@@ -48,11 +46,18 @@ class Splitter(nn.Module):
 		for layer in self.feature_layers:
 			x1 = layer(x1)
 			x2 = layer(x2)
+
 		x = torch.cat((x1,x2), dim=1)
-		for layer in self.merged_layers:
-			x = layer(x)
-			
-		return x
+		x = self.merged_fc(x)
+		x = self.activation(x)
+		line = self.output_line(x)
+		line = self.activation(line)
+
+		x = torch.cat((x,line), dim=1)
+		v = self.output_value(x)
+		v = self.output_value(v)
+
+		return line, v
 
 	def calculateReward(points, c, A, gt):
 		x1, y1, x2, y2 = points
